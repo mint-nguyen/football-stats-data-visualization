15bb3e721c15963e5355d7088249d1770587a3f2
mintnguyen.pa <mintnguyen.pa@gmail.com>
2022-04-15 12:51:57 -0700

    feat: parse csv string[][]

--- build/index.js
+++ build/index.js
@@ -4,7 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = __importDefault(require("fs"));
-const matches = fs_1.default.readFileSync("football.csv", {
+const matches = fs_1.default
+    .readFileSync("football.csv", {
     encoding: "utf-8",
+})
+    .split("\n")
+    .map((row) => {
+    return row.split(",");
 });
 console.log(matches);
